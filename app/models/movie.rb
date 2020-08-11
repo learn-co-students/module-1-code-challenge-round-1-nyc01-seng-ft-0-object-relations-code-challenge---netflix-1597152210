ea222dbaf3c5464a31343441a8e372b2b1d80cda
kain <kaindoneson@gmail.com>
2020-08-11 10:57:54 -0400

    finished!

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -21,11 +21,25 @@ class Movie
   end
 
   def average_rating
-    reviews.reduce(0){|sum, r| sum + r.rating} / reviews.length # so its working as ints and not floats btw Idk so it will round unless I coerce ratings to a float in the init for review
+    if reviews.length == 0
+      puts "movie #{self.title} has no reviews"
+      0
+    else
+      reviews.reduce(0){|sum, r| sum + r.rating} / reviews.length # so its working as ints and not floats btw Idk so it will round unless I coerce ratings to a float in the init for review
+    end
   end
+  #hey if there is no review for a movie this 0 errors out since u cant divide by 0 I added a condtional for it
+  # I thouight this was important because it chain breaks Movie.highest_rated if there is a movie with no review
+  # you have to return a 0 instead of nil after the puts because reduce cant compare ints to nil
+  # because the puts in average rating it will also list all the movies that have no reviews in highest rating before prining the highest
+  # reviewd movie
 
   def self.highest_rated
-    Movie.all.reduce(0){|highest_rating, m| highest_rating < m.average_rating ? (m.average_rating) : (highest_rating)}
+    r = Movie.all.reduce(0){|highest_rating, m| highest_rating < m.average_rating ? (m.average_rating) : (highest_rating)}
+    m = Movie.all.find{|m| m.average_rating == r}
+    puts "Movie #{m.title} has the highest rating of #{r}";
+    m;
+
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -9,9 +9,24 @@ class Review
     attr_accessor :viewer, :movie, :rating;
 
     def initialize(viewer, movie, rating)
-        @viewer = viewer;
-        @movie = movie;
-        @rating = rating;
-        self.class.all.push(self);
+        query = Review.all.find{|r| r.viewer == viewer and r.movie == movie and r.rating == rating};
+        if query
+            puts 'This Review Exists already stop trying to boost ur own movie bro'
+        else
+            @viewer = viewer;
+            @movie = movie;
+            @rating = rating;
+            self.class.all.push(self);
+        end
     end
 end
+
+
+# so I think because Review has a third element called rating we can actually check for duplicares in the initalize when we call new.
+# so yesterday with ride we couldnt tell if it was a duplicare based on just passenger, distance, and driver since it could just be
+# the driver doing a equal length drive with the same passenger
+# however, since a review for a movie from the same person with the exact same rating twice would be like rating boosting and an actual duplicate
+# so im going to fix this in the init...Hopefully it doesnt break everything else.
+
+# if u try and make a duplicate now it will shoot out an empty obj but it will not add it the class var for reviews or assiote it 
+# with anything
