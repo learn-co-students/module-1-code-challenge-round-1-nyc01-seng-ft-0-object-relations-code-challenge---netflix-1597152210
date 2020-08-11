9b815291e387e7e137ed09aa42fee5cf465761fe
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-11 09:41:49 -0400

    created testing instances

--- tools/console.rb
+++ tools/console.rb
@@ -10,9 +10,25 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+###### MOVIES ######
+m1 = Movie.new("The Shining")
+m2 = Movie.new("Rocky Horror Picture Show")
+m3 = Movie.new("The Aviator")
+m4 = Movie.new("Time")
 
+###### VIEWERS ######
+v1 = Viewer.new("sable1183")
+v2 = Viewer.new("judy4you")
+v3 = Viewer.new("val_of_the_dolls")
+v4 = Viewer.new ("blade")
 
-
+###### REVIEWS ######
+r1 = Review.new(m1, v1, 5)
+r2 = Review.new(m2, v2, 2)
+r3 = Review.new(m3, v3, 3)
+r4 = Review.new(m4, v4, 1)
+r5 = Review.new(m1, v2, 4)
+r6 = Review.new(m3, v4, 2)
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
 #binding.pry
