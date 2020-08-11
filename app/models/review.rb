1a47bf75d3946f4c485f2494ec194a8c0e78ef56
slurio <samantha.lurio@gmail.com>
2020-08-11 09:41:56 -0400

    update

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,17 @@
 class Review
+    attr_accessor :viewer, :movie, :rating
+    
+    @@all = []
 
+    def initialize(viewer, movie, rating) #rating is a number
+        @viewer = viewer
+        @movie = movie
+        @rating = rating
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
