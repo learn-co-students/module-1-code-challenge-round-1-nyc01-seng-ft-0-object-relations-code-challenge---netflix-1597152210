7d9b0bdf1cea0c4d2c0784b8aa19ea7e667171ac
AJavier55 <aliciajavier5@gmail.com>
2020-08-11 09:47:59 -0400

    boilerplate/ consolerb variables created

--- tools/console.rb
+++ tools/console.rb
@@ -9,8 +9,18 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+yeah = Movie.new("Yeah")
+moovie = Movie.new("Moovie")
+sitting = Movie.new("Sitting")
 
+cool_guy = Viewer.new("cool_guy")
+movie_watcher = Viewer.new("movie_watcher")
+reviewer22 = Viewer.new("reviewer22")
 
+new_review = Review.new(cool_guy, yeah, 2)
+other_review = Review.new(movie_watcher, moovie, 5)
+review2 = Review.new(reviewer22, moovie, 1)
+review555 = Review.new(cool_guy, sitting, 6)
 
 
 
