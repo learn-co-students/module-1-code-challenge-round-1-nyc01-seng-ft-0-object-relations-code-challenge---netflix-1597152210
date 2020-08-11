c3a3b0fae9c6b3b958a637331dbcfcc3c87e4dec
royFloresNyc <royfloresnyc@gmail.com>
2020-08-11 10:30:45 -0400

    Aggregate and Association Methods in Viewer

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -19,5 +19,26 @@ class Viewer
   def reviewed_movies
     reviews.map { |review| review.movie}
   end 
-  
+
+  def reviewed_movie?(movie) #expects a movie instance as an argument
+    reviewed = false
+    reviews.each do |review| 
+      if review.movie == movie
+        reviewed = true
+      end 
+    end 
+    reviewed
+  end 
+
+  def rate_movie(movie, rating) #expects a movie instance and a rating(number) as arguments
+    if reviewed_movie?(movie)
+      reviews.each do |review|
+        if review.movie == movie
+          review.rating = rating
+        end 
+      end 
+    else
+      Review.new(self, movie, rating)
+    end 
+  end 
 end
