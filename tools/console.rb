07ef01087e90d6bee00a0c28e007c4dfef1e091c
CanniJr <cannigialaluw@gmail.com>
2020-08-11 10:04:34 -0400

    second commit

--- tools/console.rb
+++ tools/console.rb
@@ -10,8 +10,22 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+#movie list
+ring = Movie.new("Ring")
+rush_hour = Movie.new("Rush Hour")
+pokemon = Movie.new("Pokemon")
+titanic = Movie.new("Titanic")
 
+#viewer list
+monica = Viewer.new("Monica")
+chandler = Viewer.new("Chandler")
+fred = Viewer.new("Fred")
+jose = Viewer.new("Jose")
 
+#reviews
+rev1 = Review.new(monica, titanic, 9)
+rev2 = Review.new(fred, pokemon, 7)
+rev3 = Review.new(chandler, ring, 2)
 
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
