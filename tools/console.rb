31416101003b2a6ddfe127da50fbd4b86f6d0dca
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-08-11 09:39:54 -0400

    movie class

--- tools/console.rb
+++ tools/console.rb
@@ -1,14 +1,16 @@
 # require and load the environment file
 require_relative '../config/environment.rb'
+require "pry"
 
 # call this method to reload your models as you write code
 def reload
   load 'config/environment.rb'
 end
 
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+avatar = Movie.new("Avatar")
+oldoy = Movie.new("Old Boy")
+predator = Movie.new("Predator")
+
 
 
 
