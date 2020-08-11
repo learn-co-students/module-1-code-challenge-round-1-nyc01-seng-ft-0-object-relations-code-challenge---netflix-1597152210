743d2fda9c11e9ecce12ac9f74d9ccb35fc878f5
TeddyBradsher <teddybradsher@gmail.com>
2020-08-11 10:16:24 -0400

    Panic

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,22 @@ class Viewer
   def self.all
     @@all
   end
+
+  def reviews
+    Review.all.select do |rev|
+      rev.viewer == self
+    end
+  end
+
+  def reviewed_movies
+    self.reviews.map do |view|
+      view.movie
+    end
+  end
+
+  
+
+
+
   
 end
